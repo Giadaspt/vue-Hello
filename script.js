f61d22b20a2186a25367b1f276d59f75c7b5311d
Giadaspt <saputo.giada@gmail.com>
2021-11-10 17:53:58 +0100

    prove

--- script.js
+++ script.js
@@ -6,9 +6,36 @@ const root = new Vue({
     greeting: "Hello World",
     add: '',
     image: '<img class="image" src="jae-park-7GX5aICb5i4-unsplash.jpg">',
-    
+    color: '',
+    color2: 'violet',
+    count: 0,
   },
 
+
+  methods: {
+
+    miaFunct(){
+      this.color = 'green';
+      console.log('click');
+    },
+
+    miaFunct2(){
+      this.color = 'violet';
+      console.log('click2');
+    },
+
+    miaFunct3(colore){
+      this.color = colore;
+      console.log('click3');
+    },
+
+    funzioneKeyup(){
+      ++ this.count;
+      if(this.count >= 10){
+        console.log('10 letter');
+      }
+    }
+  }
 });
 
 
